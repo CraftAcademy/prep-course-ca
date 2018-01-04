2e6b3776a55d85ddfeb6942b57218028382da213
Raoul DIFFOUO <diraulo@gmail.com>
2018-01-04 22:05:12 +0200

    Initial commit - Scaffold Gatsby project
    Generate gatsby site using the
    [gatsby-starter-docs](https://github.com/ericwindmill/gatsby-starter-docs)

---
+++ README.md
@@ -0,0 +1,27 @@
+## Coding as a Craft Bootcamp prep
+ **A prep course for junior web developers.**
+
+### Coding as a Craft - in 12 weeks
+Craft Academy is a 12-week intensive, practice-oriented course for aspiring junior developers. The course is delivered as a coding bootcamp by Craft Academy in Gothenburg, Sweden. It gives you the basics you need to create, develop and launch web-based applications.
+
+The aim is to teach you the fundamentals of web development, enabling you to hit the ground running – and keep running. But what is important is that modern engineering practices are considered to be part of this foundation. As a student, you'll work from day one with **automated testing**, **continuous integration and deployment**, and other essential skills. This means that when you enter the workforce and are faced with a problem, your instinct will be to solve intelligently rather than just hacking a solution together.
+
+### Open Source
+This publication contains the course materials we ask all our future students to go through. You are free to use it for your personal learning needs even if you are not attending our bootcamp; we encourage that.
+
+**It is, however, not allowed to use this material in a commercial context without our written consent. It is not okay and it is against the very meaning of open source.**
+
+If you have any questions, feel free to contact us at info@craftacademy.se
+
+
+**Thomas Ochman**<br>
+Gothenburg, December 2016<br>
+www.craftacademy.se
+
+**Disclaimer: This is very much a Work In Progress (WIP) and we will be pushing content regularly so please use the online version if you can or make sure you download/update the pdf, mobi or epub versions regularly.**
+
+
+
+![Logo](https://assets.craftacademy.se/images/logo/logo-with-tagline_small.png "Craft Academy by Pragmatic Sweden AB")
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Craft Academy Bootcamp</span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Pragmatic Sweden</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
---
+++ data/SiteConfig.js
@@ -0,0 +1,48 @@
+module.exports = {
+  blogPostDir: "posts", // The name of directory that contains your posts.
+  lessonsDir: "lessons", // The name of the directory that contains lessons or docs.
+  siteTitle: "Coding as a Craft Bootcamp PrepCourse", // Site title.
+  siteTitleAlt: "PrepCourse material for Craft Academy students", // Alternative site title for SEO.
+  siteLogo: "https://assets.craftacademy.se/images/logo/graphic_only_logo_white.png", // Logo used for SEO and manifest.
+  siteUrl: "https://prepcourse.craftacademy.se", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "PrepCourse material for Craft Academy students.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteFBAppID: "xxxxxxxxxx", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-xxxxxxxx-x ", // GA tracking ID.
+  disqusShortname: "ca-disqus-shortname", // Disqus shortname.
+  postDefaultCategoryID: "bootcamp", // Default category for posts.
+  userName: "Craft Academy", // Username to display in the author segment.
+  userTwitter: "CraftAcademySE", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Sweden, SE", // User location to display in the author segment.
+  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userDescription: "All about me!", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/craftacademy",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Medium",
+      url: "https://medium.com/craft-academy",
+      iconClassName: "fa fa-medium"
+    },
+    {
+      label: "Instagram",
+      url: "https://instagram.com/craft_academy",
+      iconClassName: "fa fa-envelope"
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/CraftAcademySE",
+      iconClassName: "fa fa-twitter"
+    }
+  ],
+  copyright: "Copyright © 2018. Craft Academy", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  // TODO: Move this literally anywhere better.
+  toCChapters: ["", "Chapter 1", "Chapter 2"] // Used to generate the Table Of Contents. Index 0 should be blank.
+};
